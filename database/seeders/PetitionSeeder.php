a78e12e7ecb4d446d7a9a9ba9198ba0cbae4bfd3
Rawda Yasser <rawdayasserg@gmail.com>
2023-06-22 02:58:28 +0300

    Feat: add petition api endpoints

--- database/seeders/PetitionSeeder.php
+++ database/seeders/PetitionSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use App\Models\Petition;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PetitionSeeder extends Seeder
 {
